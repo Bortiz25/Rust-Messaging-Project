267f70204d455762baf80904e33a532745cc7f99
LoganMartinez <lmartin9@trinity.edu>
2024-02-27 17:15:19 -0600

    added dummy login endpoint

--- messaging-project-server/src/handlers.rs
+++ messaging-project-server/src/handlers.rs
@@ -1,16 +1,45 @@
-use warp::reject;
 use super::models::User;
 use std::sync::Arc;
 use sqlx::PgPool;
+use warp::{Reply, Rejection, reply, reject, http::{StatusCode, Response}};
+use super::routes;
+use serde_json::json;
+
 
 #[derive(Debug)]
 struct DatabaseError;
 impl reject::Reject for DatabaseError {}
 
-pub async fn get_user(user_id: i32, pool: Arc<PgPool>) -> Result<impl warp::Reply, warp::Rejection> {
+pub async fn get_user(user_id: i32, pool: Arc<PgPool>) -> Result<impl Reply, Rejection> {
   let user_result = sqlx::query_as!(User, "SELECT * FROM users WHERE user_id = $1", user_id).fetch_one(&*pool).await;
   match user_result {
-    Ok(user) => Ok(warp::reply::json(&user)),
+    Ok(user) => Ok(reply::json(&user)),
     Err(_) => Err(reject::custom(DatabaseError))
   }
+}
+
+
+pub struct LoginResponseBody {
+  token: String,
+}
+impl Reply for LoginResponseBody {
+  fn into_response(self) -> reply::Response {
+    let body = json!({
+      "token": self.token,
+    }).to_string();
+
+    Response::builder()
+      .header("Content-Type", "application/json")
+      .body(warp::hyper::Body::from(body))
+      .unwrap()
+  }
+}
+
+pub async fn login_handler(login_request: routes::LoginRequestBody, pool: Arc<PgPool>) -> Result<reply::WithStatus<LoginResponseBody>, Rejection> {
+  login(login_request.username, login_request.password, pool).await
+}
+
+pub async fn login(username: String, password: String, pool: Arc<PgPool>) -> Result<reply::WithStatus<LoginResponseBody>, Rejection> {
+  let response_body = LoginResponseBody {token: String::from("example_token")};
+  Ok(reply::with_status(response_body, StatusCode::CREATED))
 }
--- messaging-project-server/src/routes.rs
+++ messaging-project-server/src/routes.rs
@@ -1,17 +1,33 @@
-use warp::Filter;
+use warp::{Filter, Reply, Rejection};
 use crate::handlers;
 use std::sync::Arc;
 use sqlx::PgPool;
+use serde::Deserialize;
 
-pub fn routes(pool: Arc<PgPool>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-  get_user(pool)
+pub fn routes(pool: Arc<PgPool>) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+  get_user(pool.clone())
+    .or(login(pool.clone()))
 }
 
-fn get_user(pool: Arc<PgPool>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+fn get_user(pool: Arc<PgPool>) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
   warp::path!("users" / i32)
     .and(warp::get())
     .and(with_db(pool))
     .and_then(handlers::get_user)
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequestBody {
+  pub username: String,
+  pub password: String
+}
+
+fn login(pool: Arc<PgPool>) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+  warp::path!("login")
+    .and(warp::post())
+    .and(warp::body::json::<LoginRequestBody>())
+    .and(with_db(pool))
+    .and_then(handlers::login_handler)
 }
 
 fn with_db(pool:Arc<PgPool>) -> impl Filter<Extract = (Arc<PgPool>,), Error = std::convert::Infallible> + Clone {
