09df23623be3dbee62bcdcb82b20c231b26ade4f
LoganMartinez <lmartin9@trinity.edu>
2024-02-29 11:58:02 -0600

    removed unused import

--- messaging-project-server/src/handlers.rs
+++ messaging-project-server/src/handlers.rs
@@ -1,7 +1,7 @@
 use super::models::User;
 use std::sync::Arc;
 use sqlx::PgPool;
-use warp::{Filter, Reply, Rejection, reply, reject, http::{StatusCode, Response}};
+use warp::{ Reply, Rejection, reply, reject, http::{StatusCode, Response}};
 use super::routes;
 use serde_json::json;
 
@@ -11,7 +11,9 @@ struct DatabaseError;
 impl reject::Reject for DatabaseError {}
 
 pub async fn get_user(user_id: i32, pool: Arc<PgPool>) -> Result<impl Reply, Rejection> {
-  let user_result = sqlx::query_as!(User, "SELECT * FROM users WHERE user_id = $1", user_id).fetch_one(&*pool).await;
+  let user_result = sqlx::query_as!( User, 
+    "SELECT * FROM users WHERE user_id = $1", user_id
+  ).fetch_one(&*pool).await;
   match user_result {
     Ok(user) => Ok(reply::json(&user)),
     Err(_) => Err(reject::custom(DatabaseError))
