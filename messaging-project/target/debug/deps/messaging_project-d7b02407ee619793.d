f357297166ad8cd44a6752f9690c0cd4d9c8b805
LoganMartinez <lmartin9@trinity.edu>
2024-03-01 16:49:36 -0600

    removed target files from git cache

--- messaging-project/target/debug/deps/messaging_project-d7b02407ee619793.d
+++
@@ -1,5 +0,0 @@
-/Users/brandonortiz/ClassProjects/NextGenProgramming/rust/rust-messaging-project/messaging-project/target/debug/deps/libmessaging_project-d7b02407ee619793.rmeta: src/lib.rs
-
-/Users/brandonortiz/ClassProjects/NextGenProgramming/rust/rust-messaging-project/messaging-project/target/debug/deps/messaging_project-d7b02407ee619793.d: src/lib.rs
-
-src/lib.rs:
